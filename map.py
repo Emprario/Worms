1f5a50bca3658b4fc70201d966cfde1a8a6d50e1
Emprario <pierre.cameleri@free.fr>
2024-01-31 22:16:06 +0100

    Méthode de chargement d'une map

--- map.py
+++ map.py
@@ -4,7 +4,10 @@ La méthode de gestion des maps reste à déterminer
     - Comment la charger depuis un fichier externe (assets/map/??.map)
     - Comment représenter dans le programme de la matière
 """
+from CONSTS import MAP_KEYWORD, DEFAULT_DENSITY
+from physics import get_circle
 from weapons import Weapon
+import pygame
 
 
 class Map:
@@ -12,14 +15,37 @@ class Map:
 
     def __init__(self, mappath: str):
         """Chargement de la map dans un objet (chargement logique)."""
-        pass
+        self.map: list[list[tuple[int, int]]] = []
+        with open(mappath, "r") as file:
+            for line in file:
+                if line == MAP_KEYWORD + '\n':
+                    self.map.append([])
+                else:
+                    try:
+                        spline = line.split()
+                        if int(spline[0]) < 0 or int(spline[1]) < 0:
+                            raise ValueError
+                        self.map[-1].append((int(spline[0]), int(spline[1])))
+                    except (ValueError, IndexError):
+                        if len(line) > 0 and line[-1] == '\n':
+                            line = line[:-1]
+                        else:
+                            line = line
+                        raise ValueError(
+                            f"Le valeur de '{line}' ne peut être convertie en coordonnée, est-il bien composer de deux"
+                            f" entiers positif ?"
+                        )
+        for i in range(len(self.map)):
+            if len(self.map[i]) < 3:
+                raise ValueError(f'Map {i + 1} incomplète est composé que de {len(self.map[i])} éléments')
 
-    def destroy_map(self, impact: tuple[int, int], weapon: Weapon, power: float):
+        self.polygones: list[pygame.Rect] = []
+
+    def destroy_map(self, impact: tuple[int, int], weapon: Weapon):
         """
         Enregistre une destruction de la map, utilise un point d'impact et une arme caractéristique ansi qu'une puissance
         :param impact: Position (x, y)
         :param weapon: Arme utilisée
-        :param power: Puissance de l'impact (selon arme)
         """
         pass
 
@@ -45,3 +71,10 @@ class TriPixel:
     def pg_flip(self):
         """Pygame Filp : Fonction d'update spécifique au TriPixel"""
         pass
+
+
+if __name__ == "__main__":
+    print("Suite de test.")
+    mapobj = Map("./assets/map/dummy.map")
+    print(mapobj.map)
+    print(list(get_circle(DEFAULT_DENSITY, (0, 0), 10)))
