2f56e1fc4086554318fd8dd2058ca7aa9de630dc
Emprario <pierre.cameleri@free.fr>
2024-01-31 22:16:38 +0100

    hotfix: ajout d'un power sur les armes `Weapon`

--- weapons.py
+++ weapons.py
@@ -3,16 +3,18 @@
 
 class Weapon:
     """Arme générique"""
+
     def __init__(self, texture: str):
         """
         Constructeur ...
         :param texture: Chemin vers la texture
         """
-        pass
+        self.power = 10
 
     def pg_flip(self):
         """Pygame Filp : Fonction d'update spécifique au worm"""
         pass
+
 
 class StGrenade(Weapon):
     """Exemple d'objet spécifique : la St Grenade"""
