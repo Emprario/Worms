840219bb59678ab2bac84e07b648515e0a7c048f
LucasMonnais <lucas.monnais@efrei.net>
2024-05-05 17:35:40 +0200

    "fixed gravity"

--- physics.py
+++ physics.py
@@ -37,37 +37,39 @@ def translation(v_init: float, alpha: float, map: list[list[bool]], entity: Enti
     :param local_tick: Tick initiale (tick au moment du départ i.e. ce n'est pas zéro)
     :return: tuple (Arrêt du mouvment, sommes nous dans un mode de force ?, vitesse à l'impact si impact sinon None)
     """
-    prox = reduce(lambda pre, nex: pre or nex,
-                  [map[entity.x + X][entity.y + Y] for X in range(-1, 2) for Y in range(-1, 2)]
-                  )
-    if force or not prox:
-        for militick in range(0, MILITICK):
-            temp = entity.x, entity.y
-            entity.move_to(local_tick, v_init, alpha, 1 / MILITICK)
+    # prox = reduce(lambda pre, nex: pre or nex,
+    #               [map[entity.x + X][entity.y + Y] for X in range(-1, 2) for Y in range(-1, 2)]
+    #               )
+    # if force or not prox:
+    for militick in range(0, MILITICK):
+        temp = entity.x, entity.y
+        entity.move_to(local_tick, v_init, alpha, 1 / MILITICK)
 
-            # if not map[entity.x][entity.y + 1]:
-            #    force = False
-            # print(entity.x)
-            if map[entity.x][entity.y]:
-                force = False
+        # if not map[entity.x][entity.y + 1]:
+        #    force = False
+        # print(entity.x)
+        if map[entity.x][entity.y]:
+            force = False
 
-                lst = get_full_line(temp, (entity.x, entity.y))
-                if lst[0] == temp:
-                    # Extérieur à 0
-                    step = 1
-                    i = 0
-                else:
-                    # Exterieur à la fin
-                    step = -1
-                    i = len(lst) - 1
+            lst = get_full_line(temp, (entity.x, entity.y))
+            if lst[0] == temp:
+                # Extérieur à 0
+                step = 1
+                i = 0
+            else:
+                # Exterieur à la fin
+                step = -1
+                i = len(lst) - 1
 
+            impact = lst[i]
+            while (not map[lst[i][0]][lst[i][1]]) and 0 <= i < len(lst):
                 impact = lst[i]
-                while (not map[lst[i][0]][lst[i][1]]) and 0 <= i < len(lst):
-                    impact = lst[i]
-                    i += step
+                i += step
 
-                entity.x, entity.y = impact
-                break
+            entity.x, entity.y = impact
+
+            x, y = entity.get_speed(local_tick, v_init, alpha)
+            return True, force, (x ** 2 + y ** 2) ** 0.5
         return False, force, None
     x, y = entity.get_speed(local_tick, v_init, alpha)
     return True, force, (x ** 2 + y ** 2) ** 0.5
