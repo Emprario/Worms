321f46658bbc4b06aaa7a858d1694cd38ff9ce55
Antonin1 <antonin.croquette@gmail.com>
2024-05-13 08:32:43 +0200

    rebase

--- weapon.py
+++ weapon.py
@@ -2,53 +2,94 @@ import pygame
 
 from entity import Entity
 from math import pi
+from math import sin
+from math import cos
 from utils import get_circle
 
 
-# path = "assets/map/01.map"
-# map = TileMap(path)
+class Fleche(Entity):
+    def __init__(self, x: int, y: int, image: str, angle:int,  size: int):
+        super().__init__(x, y)
+        self.original = pygame.image.load(image)
+        self.x_base=self.x
+        self.y_base=self.y
+        self.angle= angle
+        self.original = pygame.transform.scale(self.original, (size, size))
+        self.image = pygame.transform.rotate(self.original, angle)
+    def rotate(self, angle):
+        self.image = pygame.transform.rotate(self.original, angle+self.angle)
+    def move_with_rota(self,angle):
+        self.x=self.x_base+cos(angle)*100
+        self.y=self.y_base+sin(angle)*100
 
-# flags = pygame.FULLSCREEN | pygame.HWSURFACE
-# SCREEN = pygame.display.set_mode(map.dimensions, flags)
 
-
-# WHITE = (255, 255, 255)
-# BLACK = (0, 0, 0)
-RED = (255, 0, 0)
 
 # all_moves: list[tuple[float, float, list[list[bool]], coordinate, Entity, bool, int, Callable]] = list()
 class Charg_bar(Entity):
     def __init__(self, x: int, y: int):
         super().__init__(x, y)
         self.image = pygame.Surface((0, 15))
+        self.image.fill((255, 0, 0))
         self.taille = 0
         self.agrandissement=False
     def up_taille(self,vitesse: int):
         self.taille += vitesse
-        self.image = self.image = pygame.Surface((self.taille, 30))
+        self.image = self.image = pygame.Surface((self.taille, 15))
+
+        self.image.fill((255, 0, 0))
     def reset_taille(self):
         self.taille=0
-        self.image = self.image = pygame.Surface((self.taille, 30))
+        self.image = self.image = pygame.Surface((self.taille, 15))
 
 
-class Bazooka(Entity):
-    def __init__(self, x: int, y: int, image: str, rotation: int):
+class Weapon(Entity):
+    def __init__(self, x: int, y: int, image: str, rotation: int, angle:int,  size: int):
         super().__init__(x, y)
         self.original = pygame.image.load(image)
-        self.angle= -30
-        self.original = pygame.transform.scale(self.original, (50, 50))
-        # resized = pygame.transform.scale(self.original, (50, 50))
-        self.image = pygame.transform.rotate(self.original, rotation)
-        # self.image = pygame.Surface((30, 30), pygame.SRCALPHA)
-        # self.rect = self.image.get_rect(center=(x, y))
-        # self.image.blit(rotated_image, self.rect)
+        self.angle= angle
+        self.original = pygame.transform.scale(self.original, (size, size))
+        self.image = pygame.transform.rotate(self.original, angle+rotation)
+        self.x_base=self.x
+        self.y_base=self.y
 
+class Bazooka(Weapon):
+    def __init__(self, x: int, y: int, image: str, rotation: int, angle:int,  size: int):
+        super().__init__(x, y, image, rotation, angle, size)
     def rotate(self, angle):
-        # Rotation de l'image originale pour éviter la dégradation
-        self.angle+=angle
-        self.image = pygame.transform.rotate(self.original, self.angle)
-        # Réajuster le rectangle pour centrer l'image
-        # self.rect = self.image.get_rect(center=(self.rect.center))
+        self.image = pygame.transform.rotate(self.original, angle+self.angle)
+    def move_with_rota(self,angle):
+        self.x=self.x_base+cos(angle)*20
+        self.y=self.y_base+sin(angle)*20
+
+
+class Sniper(Weapon):
+    def __init__(self, x: int, y: int, image: str, rotation: int, angle:int,  size: int):
+        super().__init__(x, y, image, rotation, angle, size)
+    def rotate(self, angle):
+        self.image = pygame.transform.rotate(self.original, angle+self.angle)
+    def move_with_rota(self,angle):
+        self.x=self.x_base+cos(angle)*20
+        self.y=self.y_base+sin(angle)*20
+
+
+class Grenade(Weapon):
+    def __init__(self, x: int, y: int, image: str, rotation: int, angle:int,  size: int):
+        super().__init__(x, y, image, rotation, angle, size)
+    def rotate(self, angle):
+        self.image = pygame.transform.rotate(self.original, angle+self.angle)
+    def move_with_rota(self,angle):
+        self.x=self.x_base+cos(angle)*20
+        self.y=self.y_base+sin(angle)*20
+
+class Grenade_frag(Weapon):
+    def __init__(self, x: int, y: int, image: str, rotation: int, angle:int,  size: int):
+        super().__init__(x, y, image, rotation, angle, size)
+    def rotate(self, angle):
+        self.image = pygame.transform.rotate(self.original, angle+self.angle)
+    def move_with_rota(self,angle):
+        self.x=self.x_base+cos(angle)*20
+        self.y=self.y_base+sin(angle)*20
+
 
 
 class Projectile(Entity):
@@ -61,6 +102,8 @@ class Projectile(Entity):
         self.launched = False
         self.map_destroy_stack = map_destroy_stack
         self.rebond = False
+        self.original = pygame.image.load(image)
+        self.image = pygame.transform.scale(self.original, (taille, taille))
 
 
 class Pro_bazooka(Projectile):
@@ -99,6 +142,7 @@ class Pro_grenade(Projectile):
     def __init__(self, x: int, y: int, image: str, map_destroy_stack, speed: float,taille: int, rebond: bool):
         super().__init__(x, y, image, map_destroy_stack, speed,taille,rebond)
 
+
     def destroy(self, *args):
         print("blow")
         circle = get_circle(5, pos := (self.x, self.y), radius := 60)
@@ -109,6 +153,7 @@ class Pro_grenade(Projectile):
     # all_moves: list[list[float, float, list[list[bool]], Entity, bool, int, Callable]] = list()
     def add_to_move(self, movelst: list, map: list[list[bool]], tick: int, inclinaison: int, power: int):
         movelst.append([power * self.speed, inclinaison, map, self, True, tick, self.destroy])
+
 
 
 class Pro_frag_grenade(Projectile):
